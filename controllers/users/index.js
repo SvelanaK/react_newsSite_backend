0ba6d963d72c3d1a79605f469e7b45938fce4afc
Svetlana <s.kosuhina@dunice.net>
2022-07-06 14:21:26 +0300

    fix controllers(2)

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -1,9 +1,7 @@
-const { createUser } = require('../authorization/createUser');
 const { getUsersNews } = require('./getUsersNews');
 const { updateUserInfo } = require('./updateUserInfo');
 
 module.exports = {
-  createUser,
   getUsersNews,
   updateUserInfo,
 };
