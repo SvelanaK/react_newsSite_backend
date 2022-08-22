a6609c1c3f5604cf91b816cd98ef73a27f595f18
Svetlana <s.kosuhina@dunice.net>
2022-08-22 11:11:33 +0300

    added get user controller

---
+++ controllers/user/getUserPage.js
@@ -0,0 +1,41 @@
+const { News, Users } = require('../../models');
+const { RESPONSE_STATUSES } = require('../../constants');
+
+module.exports = {
+  async getUserPage(req, res) {
+    try {
+      const { id } = req.params;
+      const foundUser = await Users.findOne(
+        { where: { id } },
+      );
+
+      if (!foundUser) {
+        return res
+          .status(RESPONSE_STATUSES.BAD_REQUEST)
+          .send({ message: 'User not found' });
+      }
+
+      const usersNews = await News.findAll(
+        { where: { userId: id } },
+      );
+
+      return res
+        .status(RESPONSE_STATUSES.OK)
+        .send({
+          user: {
+            id: foundUser.id,
+            login: foundUser.login,
+            email: foundUser.email,
+            firstName: foundUser.firstName,
+            lastName: foundUser.lastName,
+            registrationDate: foundUser.createdAt,
+          },
+          usersNews,
+        });
+    } catch (error) {
+      return res
+        .status(RESPONSE_STATUSES.BAD_REQUEST)
+        .send(error.message);
+    }
+  },
+};
